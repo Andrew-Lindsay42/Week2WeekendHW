817747440477b81b58edd2431bb822dccd605417
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-26 15:57:20 +0000

    Initalisation tests for Room class

--- tests/room_test.py
+++ tests/room_test.py
@@ -3,4 +3,18 @@ from src.guest import Guest
 from src.song import Song
 
 class TestRoom(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.waterloo = Song("Waterloo", "ABBA", "Europop")
+        self.september = Song("September", "Earth, Wind & Fire", "R&B")
+        self.seventies_playlist = [self.waterloo, self.september]
+        self.room = Room("70's classics", self.seventies_playlist, [])
+
+    def test_room_has_name(self):
+        self.assertEqual("70's classics", self.room.name)
+
+    def test_room_has_playlist(self):
+        self.assertEqual(2, len(self.room.playlist))
+
+    def test_room_has_guests(self):
+        self.assertEqual(0, len(self.room.guests))
