fb6cae493b1159b434b7963f77a37c6e106a2e61
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-11-26 15:46:13 +0000

    Initalisation test for Guest class

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,4 +2,9 @@ import unittest
 from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.andrew = Guest("Andrew")
+
+    def test_guest_has_name(self):
+        self.assertEqual("Andrew", self.andrew.name)
